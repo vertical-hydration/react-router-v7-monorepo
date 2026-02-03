5e025462c36c7c87851cf2c319636f7766293af0
MarkMutt <lleasy8@gmail.com>
2026-02-03 14:20:11 -0500

    chore(db): attempting to setup db:studio

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,7 +1,27 @@
 import type { Config } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+const LOCAL_DB_PATH = String(process.env.LOCAL_DB_PATH || '');
+
+
+export default defineConfig(
+  process.env.LOCAL_DB_PATH // Check if LOCAL_DB_PATH is set
+  ?
+ {
   schema: "./src/schema/*.ts",
   out: "./migrations",
   dialect: "sqlite",
-} satisfies Config;
+  dbCredentials:{
+    url: LOCAL_DB_PATH
+  }
+
+}
+:{
+  schema: "./src/schema/*.ts",
+  out: "./migrations",
+  dialect: "sqlite",
+  dbCredentials:{
+    url:LOCAL_DB_PATH
+  }
+}
+);
